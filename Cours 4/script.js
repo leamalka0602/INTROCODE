e80413e3f6d98a2f5f97ecd44edb405310aa27d5
leamalka0602 <36660436+leamalka0602@users.noreply.github.com>
2018-04-10 21:47:37 +0200

    Push pour déblocage
    Monsieur,
    Malgré mes recherches pour détecter le problèmes, je ne parviens pas à identifier les raisons pour lesquelles :
    - le titre "Bienvenue, soyez prêt (...)" ne s'affiche pas
    - la boite de dialogue qui demande de saisir son prénom ne s'affiche pas
    
    Merci pour votre aide !

--- Cours 4/script.js
+++ Cours 4/script.js
@@ -1,8 +1,22 @@
-
 $( document ).ready(function() {
-  function welcome() {
+
+function welcome() {
    $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
 };
-welcome();
-})
+welcome ();
 
+
+function getUsername() {
+    //affiche une boite de dialogue 
+    var  user = prompt("Veuillez rentrez votre nom:", "");
+    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    return user
+};
+
+ function welcome() {
+    var name= getUsername();
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+
+};
+// Appel la fonction
+welcome();
